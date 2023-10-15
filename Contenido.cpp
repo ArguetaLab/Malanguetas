f2fe84d45cfdbfad65bc597ae1bc52745477910c
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2023-10-14 18:28:38 -0600

    Update Contenido.cpp

--- Contenido.cpp
+++ Contenido.cpp
@@ -3,6 +3,10 @@
 #include <fstream>
 #include <map>
 #include <iostream>
+#include <unordered_map>
+#include <sstream>
+
+
 using namespace std;
 
 
@@ -10,33 +14,71 @@ class Contenido{
 // Iniciamos las variables
 private:
     string sabor;
-    int precio;
     float cantidad;
-    int popularidad;
-    map<std::string, int> saboresCantidad;
+    // Aquí se está aplicando estructura de datos ya que estamos usando hash maps
+    unordered_map<string, unordered_map<string, int>> saboresCantidad;
+    
 
 public:
+    void cargarDatosDesdeTXT(const string& archivo) {
+        ifstream file(archivo);
 
-    void agregarSabor(const string& sabor, int cantidad) {
-        saboresCantidad[sabor] = cantidad;
-        
+        string line;
+        getline(file, line); // Leer la primera línea (encabezados)
+
+        istringstream iss(line); // Usar un stringstream para leer los encabezados
+        vector<string> encabezados;
+        string token;
+
+        while (getline(iss, token, ',')) {
+            encabezados.push_back(token);
+        }
+
+        while (getline(file, line)) {
+            istringstream iss(line);
+            string fecha, sabor;
+            int cantidad;
+
+            if (getline(iss, fecha, ',')) {
+                for (size_t i = 1; i < encabezados.size(); i++) {
+                    if (getline(iss, line, ',')) {
+                        cantidad = stoi(line);
+                        saboresCantidad[fecha][encabezados[i]] = cantidad;
+                    }
+                }
+            }
+        }
+
+        file.close();
     }
 
-    int buscarSabor(const std::string& sabor) {
-        // Para este caso ocupamos las búsqueda binaria
-        // El usuario ingresa un sabor y el programa regresa el sabor junto con la cantidad de malangas vendidas a la semana
+
+     void agregarSabor(const string& fecha, const string& sabor, int cantidad) {
+        saboresCantidad[fecha][sabor] = cantidad;
+    }
+
+
+int buscarSabor(const string& fecha, const string& sabor) {
+    if (saboresCantidad.find(fecha) != saboresCantidad.end()) {
+        unordered_map<string, int>& sabores = saboresCantidad[fecha];
+
+        auto it = sabores.find(sabor);
+        if (it != sabores.end()) {
+            int cantidad = it->second;
+            cout << "La cantidad de " << sabor << " vendida en la fecha " << fecha << " es: " << cantidad << endl;
+            return cantidad;
+        }
+
+        // Si no se encuentra el sabor, realizamos una búsqueda binaria
+        vector<pair<string, int>> saboresVector(sabores.begin(), sabores.end());
         int low = 0;
-        int high = saboresCantidad.size() - 1;
+        int high = saboresVector.size() - 1;
 
         while (low <= high) {
-            int mid = (low + high) / 2;
-
-            // Convertir el mapa en un vector para acceder por índice
-            std::vector<std::pair<std::string, int>> saboresVector(saboresCantidad.begin(), saboresCantidad.end());
-
+            int mid = low + (high - low) / 2;
             if (sabor == saboresVector[mid].first) {
-                int cantidad = saboresVector[mid].second; // Obtener la cantidad si el sabor existe
-                std::cout << "La cantidad de " << sabor << " es: " << cantidad << std::endl;
+                int cantidad = saboresVector[mid].second;
+                cout << "La cantidad de " << sabor << " vendida en la fecha " << fecha << " es: " << cantidad << endl;
                 return cantidad;
             } else if (sabor < saboresVector[mid].first) {
                 high = mid - 1;
@@ -44,41 +86,54 @@ public:
                 low = mid + 1;
             }
         }
-
-        std::cout << sabor << " no se encontró en el contenido." << std::endl;
-        return -1; // Retorna un valor negativo si el sabor no se encontró
     }
 
-    
-    void ordenarSaboresPorCantidad() {
-        // Crear un vector de pares (nombre del sabor, cantidad) a partir del mapa
-        vector<pair<string, int>> saboresVector(saboresCantidad.begin(), saboresCantidad.end());
+    cout << "Sabor no encontrado en la fecha " << fecha << endl;
+    return -1;
+}
 
-        // Ordenar el vector de pares por las cantidades usando el algoritmo de inserción
-        for (int i = 1; i < saboresVector.size(); i++) {
-            // Tomar el elemento actual que queremos insertar en su posición correcta
-            pair<string, int> claveValor = saboresVector[i];
-            int j = i - 1;
 
-            // Mover los elementos mayores que claveValor hacia adelante
-            while (j >= 0 && claveValor.second < saboresVector[j].second) {
-                saboresVector[j + 1] = saboresVector[j];
-                j--;
+
+
+
+    void ordenarSaboresPorCantidad(const string& fecha) {
+        if (saboresCantidad.find(fecha) != saboresCantidad.end()) {
+            unordered_map<string, int>& sabores = saboresCantidad[fecha];
+
+            vector<pair<string, int>> saboresVector(sabores.begin(), sabores.end());
+
+            for (int i = 1; i < saboresVector.size(); i++) {
+                pair<string, int> claveValor = saboresVector[i];
+                int j = i - 1;
+
+                while (j >= 0 && claveValor.second < saboresVector[j].second) {
+                    saboresVector[j + 1] = saboresVector[j];
+                    j--;
+                }
+
+                saboresVector[j + 1] = claveValor;
             }
 
-            // Colocar claveValor en su posición correcta
-            saboresVector[j + 1] = claveValor;
+            cout << "Sabores ordenados de menor a mayor por cantidad en la fecha " << fecha << ":" << endl;
+            for (const auto& par : saboresVector) {
+                cout << par.first << ": " << par.second << endl;
+            }
+        } else {
+            cout << "Fecha no encontrada." << endl;
         }
+}
 
-        cout << "Sabores ordenados de menor a mayor por cantidad:"<<endl;
-        for (const auto& par : saboresVector) {
-            cout<<par.first << ": " << par.second << endl; // Utiliza std::endl aquí
+    void imprimirTodoElHashMap() {
+        cout << "Contenido del HashMap:" << endl;
+        for (const auto& pairFechaSabores : saboresCantidad) {
+            const string& fecha = pairFechaSabores.first;
+            const unordered_map<string, int>& sabores = pairFechaSabores.second;
+
+            cout << "Fecha: " << fecha << endl;
+            for (const auto& par : sabores) {
+                cout << "  " << par.first << ": " << par.second << endl;
+            }
         }
     }
 
- 
 };
-
-
-
-
